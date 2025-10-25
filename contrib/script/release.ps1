e6b647fb193e0e68fa0c3d0e0f0de3d370cb8d92
Nicolas GERARD <gerardnico@gmail.com>
2025-10-25 11:49:52 +0000

    feat(release): release version 1.0.0

--- contrib/script/release.ps1
+++ contrib/script/release.ps1
@@ -1,23 +1,28 @@
+# Windows script to create the windows installer with Jreleaser
 
-# Maven installation
-# https://maven.apache.org/install.html
-winget install --id=Chocolatey.Chocolatey  -e
-choco install maven
-
+# The trick is to
+# * copy the Jlink image on Windows
+# * run the JReleaser script
+# * copy the resulting installer back in WSL
 
 # Wix 3 (not 4)
+# The Jdk 17 relies on Wix3 - ie on WiX compiler (Candle) and WiX linker (Light).
+# This wix tools are no more present in Wix4
 winget install -e --id WiXToolset.WiXToolset -v 3.14.1.8722
-
-# Package and download the SDK
-mvn '-Dmaven.test.skip=true' package -P release
-# Note to only download one jdk, we need to use the jdks:setup-jdks
-# but we need to set the download URL
 
 # Create the distributions
 .\contrib\script\jreleaser-de.ps1 assemble --debug --select-current-platform
 
-# Create the manifest files
-.\contrib\script\jreleaser-de.ps1 prepare --debug --select-current-platform -p=winget
-
-# Test
-# winget install --manifest target/jreleaser/prepare/doc-exec-installer/winget/manifests/c/Combostrap/doc-exec/1.0.0
+# Old Code
+# for a full run from Windows
+# Painfull
+#
+# Maven installation
+# https://maven.apache.org/install.html
+# winget install --id=Chocolatey.Chocolatey  -e
+# choco install maven
+#
+# Package and download the SDK
+# mvn '-Dmaven.test.skip=true' package -P release
+# Note to only download one jdk, we need to use the jdks:setup-jdks
+# but we need to set the download URL
